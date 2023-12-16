e462a1af9a63b8f5aae40ac57da4cfab477eebd8
bishar <bishar.dahir@student.moringaschool.com>
2023-12-16 16:29:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+
+
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+
+
+const result = max(10, 5, 20, 15);
